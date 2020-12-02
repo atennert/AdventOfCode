06799de3de2311f1dc54fc6477283f8530ad2f85
Andreas Tennert <mail@andreas-tennert.de>
2020-12-02 18:25:54 +0100

    feat: add day 2 solution

---
+++ day2/program.hs
@@ -0,0 +1,33 @@
+import Data.List.Split (splitOn, splitOneOf)
+import Data.List (elemIndices)
+
+convertEntry :: String -> (Int, Int, Char, String)
+convertEntry entry =
+  let [rule,pw]          = splitOn ": " entry
+      [int1,int2,letter] = splitOneOf "- " rule
+  in (read int1, read int2, head letter, pw)
+
+isPasswordValid1 :: (Int, Int, Char, String) -> Bool
+isPasswordValid1 (minCount, maxCount, letter, pw) =
+    minCount <= occurenceCount && occurenceCount <= maxCount
+  where occurenceCount = length $ elemIndices letter pw
+
+isPasswordValid2 :: (Int, Int, Char, String) -> Bool
+isPasswordValid2 (pos1, pos2, letter, pw) =
+    ((pos1 - 1) `elem` occurences) `xor` ((pos2 - 1) `elem` occurences)
+  where occurences = elemIndices letter pw
+
+xor :: Bool -> Bool -> Bool
+xor True x  = not x
+xor False x = x
+
+countCorrectPasswords :: [Bool] -> Int
+countCorrectPasswords = sum . map fromEnum
+
+evaluatePasswords :: ((Int, Int, Char, String) -> Bool) -> [String] -> Int
+evaluatePasswords f = countCorrectPasswords . map (f . convertEntry)
+
+compute :: [String] -> [Int]
+compute x = map (`evaluatePasswords` x) [isPasswordValid1, isPasswordValid2]
+
+main = interact $ unlines . map show . compute . lines
