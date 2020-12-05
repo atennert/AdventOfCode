521368bcd630500528bc63b69d5d86f544a92196
Andreas Tennert <mail@andreas-tennert.de>
2020-12-05 19:09:46 +0100

    feat: add code for day 5

---
+++ day5/program.hs
@@ -0,0 +1,24 @@
+import Data.List (sort)
+
+getSeat :: String -> (Int, Int)
+getSeat = (\(r,c) -> (conv 'B' r, conv 'R' c)) . splitAt 7
+  where conv x = snd . foldr (\l (e,s) -> if l == x then (e*2,s+e) else (e*2,s)) (1,0)
+
+getSeatId :: (Int, Int) -> Int
+getSeatId (row, col) = row * 8 + col
+
+findEmptySeat :: [Int] -> Int
+findEmptySeat (a:b:ls) | a+1 == b  = findEmptySeat (b:ls)
+                       | otherwise = a+1
+
+getLast :: [String] -> Int
+getLast = maximum . map (getSeatId . getSeat)
+
+getMySeat :: [String] -> Int
+getMySeat = findEmptySeat . sort . map (getSeatId . getSeat)
+
+compute :: [String] -> [String]
+compute x = map (`run` x) [getLast, getMySeat]
+  where run f = show . f
+
+main = interact $ unlines . compute . lines
