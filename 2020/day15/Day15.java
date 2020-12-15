fd1aebaf15d367ad9a3af27eb76f649f44ba0c1c
Andreas Tennert <mail@andreas-tennert.de>
2020-12-15 14:45:41 +0100

    refactor: optimize code for Java day 15

--- 2020/day15/Day15.java
+++ 2020/day15/Day15.java
@@ -3,14 +3,14 @@ import java.util.Map;
 
 public class Day15 {
 
-	static Map<Integer, int[]> l = new HashMap<>(2020);
+	static Map<Integer, int[]> l = new HashMap<>(10000);
 	static {
-		l.put(10, new int[]{1});
-		l.put(16, new int[]{2});
-		l.put(6, new int[]{3});
-		l.put(0, new int[]{4});
-		l.put(1, new int[]{5});
-		l.put(17, new int[]{6});
+		l.put(10, new int[]{-1, 1});
+		l.put(16, new int[]{-1, 2});
+		l.put(6, new int[]{-1, 3});
+		l.put(0, new int[]{-1, 4});
+		l.put(1, new int[]{-1, 5});
+		l.put(17, new int[]{-1, 6});
 	}
 	static int last = 17;
 
@@ -18,9 +18,7 @@ public class Day15 {
 		last = key;
 		int[] places = l.get(key);
 		if (places == null) {
-			l.put(key, new int[]{pos});
-		} else if (places.length == 1) {
-			l.put(key, new int[]{places[0], pos});
+			l.put(key, new int[]{-1, pos});
 		} else {
 			places[0] = places[1];
 			places[1] = pos;
@@ -32,7 +30,7 @@ public class Day15 {
 		while (counter < 30000000) {
 			counter++;
 			int[] places = l.get(last);
-			if (places.length == 1) {
+			if (places[0] == -1) {
 				add(0, counter);
 			} else {
 				add(places[1] - places[0], counter);
