7cd8f3bd15c26e49b562404f5447c0f773b3b785
Andreas Tennert <mail@andreas-tennert.de>
2020-12-21 14:08:27 +0100

    refactor: cleanup day 21

--- 2020/day21/program.hs
+++ 2020/day21/program.hs
@@ -13,18 +13,16 @@ part1 (ingredients, map) = show $ length $ Prelude.filter (not . (`S.member` wit
         withAllergent = M.foldr (\i s -> S.union s $ mayhaveAllergent i) S.empty map
 
 part2 :: ([Ingredient], Map Allergen [Set Ingredient]) -> String
-part2 (ingredients, map') = combine $ M.elems $ collate M.empty $ filterInAllergents $ getAllergents map'
+part2 (_, map) = combine $ M.elems $ collate M.empty $ filterInAllergents $ getAllergents map
   where mayhaveAllergent = foldr1 S.intersection
-        withAllergent = M.foldr (\i s -> S.union s $ mayhaveAllergent i) S.empty map'
+        withAllergent = M.foldr (\i s -> S.union s $ mayhaveAllergent i) S.empty map
         getAllergents = M.map (Prelude.map (S.filter (`S.member` withAllergent)))
         filterInAllergents = M.map (foldr1 S.intersection)
         collate nm om | M.null om = nm
-                      | otherwise = collate (singleIng nm om) $ cleanUp (singleIng nm om) (nonSingle om)
+                      | otherwise = collate (singleIng nm om) $ cleanUp (singleIng nm om) om
         singleIng nm = M.union nm . M.map (head . S.toList) . M.filter ((== 1) . S.size)
-        nonSingle = M.filter ((> 1) . S.size)
-        cleanUp nm om = M.map (S.filter (not . (`elem` M.elems nm))) om
+        cleanUp nm = M.map (S.filter (not . (`elem` M.elems nm))) . M.filter ((> 1) . S.size)
         combine = foldr1 (\e1 e2 -> e1 ++ "," ++ e2)
-
 
 convert :: [String] -> ([Ingredient], Map Allergen [Set Ingredient])
 convert = Prelude.foldr (analyze . splitOn " (contains ") ([], M.empty)
