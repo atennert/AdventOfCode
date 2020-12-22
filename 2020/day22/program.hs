49ba9f9d4438ee50789e7fba2c61a2c750514fbd
Andreas Tennert <mail@andreas-tennert.de>
2020-12-22 17:58:13 +0100

    refactor: beautify code a little bit

--- 2020/day22/program.hs
+++ 2020/day22/program.hs
@@ -1,19 +1,21 @@
 import Data.Bifunctor (bimap)
 
-play1 :: [Int] -> [Int] -> Int
+type Card = Int
+
+play1 :: [Card] -> [Card] -> Int
 play1 [] p2 = points p2
 play1 p1 [] = points p1
 play1 (a:p1) (b:p2) | a > b     = play1 (p1 ++ [a,b]) p2
                     | otherwise = play1 p1 (p2 ++ [b,a])
 
-play2 :: [Int] -> [Int] -> Int
+play2 :: [Card] -> [Card] -> Int
 play2 p1 p2 = points $ snd $ play2' p1 p2 []
 
-play2' :: [Int] -> [Int] -> [([Int], [Int])] -> (Int, [Int])
+play2' :: [Card] -> [Card] -> [([Card], [Card])] -> (Int, [Card])
 play2' [] p2 _ = (2, p2)
 play2' p1 [] _ = (1, p1)
 play2' (a:p1) (b:p2) rounds
-    | (a:p1,b:p2) `elem` rounds = (1, a:p1)
+    | (a:p1, b:p2) `elem` rounds = (1, a:p1)
     | (length p1 >= a) && (length p2 >= b) = if 1 == fst subgame
                                              then play2' (p1 ++ [a,b]) p2 newRounds
                                              else play2' p1 (p2 ++ [b,a]) newRounds
@@ -22,10 +24,10 @@ play2' (a:p1) (b:p2) rounds
   where subgame   = play2' (take a p1) (take b p2) []
         newRounds = rounds ++ [(a:p1, b:p2)]
 
-points :: [Int] -> Int
+points :: [Card] -> Int
 points = sum . zipWith (*) [1..] . reverse
 
-convert :: [String] -> ([Int], [Int])
+convert :: [String] -> ([Card], [Card])
 convert = bimap c (c . tail) . break null
   where c = map read . tail
 
