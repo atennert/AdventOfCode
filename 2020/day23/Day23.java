8f67352cffa191e09db0a676bf7f9b40a3a172aa
Andreas Tennert <mail@andreas-tennert.de>
2020-12-23 22:21:04 +0100

    feat: add day 23 part 2

--- 2020/day23/Day23.java
+++ 2020/day23/Day23.java
@@ -17,12 +17,28 @@ public class Day23 {
             System.out.print(cup);
         }
         System.out.println();
+
+        // part 2
+        rounds = 10_000_000;
+        int[] cups2 = new int[1000000];
+        for (i = 0; i < cups.length; i++) {
+            cups2[i] = cups[i];
+        }
+        for (; i < cups2.length; i++) {
+            cups2[i] = i+1;
+        }
+
+        result = run(cups2, rounds, cups2.length);
+        long[] oneAndTwo = findAfter1(result, cups2.length);
+        System.out.println();
+        System.out.println(oneAndTwo[0] + " * " + oneAndTwo[1]);
+        System.out.println(oneAndTwo[0] * oneAndTwo[1]);
     }
 
     private static int[] run(int[] input, int rounds, int size) {
         int[] cups = input.clone();
 
-        int current, n1, n2, n3, dest, destIdx = 0;
+        int current, n1, n2, n3, destIdx = 0;
         for (int i = 0; i < rounds; i++) {
             if (i%1000 == 0) {
                 System.out.print(i + "\r");
@@ -33,13 +49,7 @@ public class Day23 {
             n2 = cups[2];
             n3 = cups[3];
 
-            dest = getDestination(current, n1, n2, n3, size);
-            for (int j = 0; j < size; j++) {
-                if (cups[j] == dest) {
-                    destIdx = j;
-                    break;
-                }
-            }
+            destIdx = getDestinationIdx(cups, current, n1, n2, n3, size);
 
             if (destIdx != 0) {
                 shiftLeft(cups, 3, destIdx + 1);
@@ -54,15 +64,23 @@ public class Day23 {
         return cups;
     }
 
-    private static int getDestination(int current, int n1, int n2, int n3, int size) {
+    private static int getDestinationIdx(int[] cups, int current, int n1, int n2, int n3, int size) {
         int dest = current;
+        int destIdx = 0;
         do {
             dest -= 1;
             if (dest == 0) {
                 dest = size;
             }
         } while (dest == n1 || dest == n2 || dest == n3);
-        return dest;
+
+        for (int j = 0; j < size; j++) {
+            if (cups[j] == dest) {
+                destIdx = j;
+                break;
+            }
+        }
+        return destIdx;
     }
 
     private static void shiftLeft(int[] cups, int shiftSize, int length) {
@@ -81,5 +99,19 @@ public class Day23 {
         tmp = array[0];
         shiftLeft(array, 1, size);
         array[size-1] = tmp;
+    }
+
+    private static long[] findAfter1(int[] cups, int size) {
+        int oneIdx = 0;
+        long[] result = new long[2];
+        for (int i = 0; i < size; i++) {
+            if (cups[i] == 1) {
+                oneIdx = i;
+                break;
+            }
+        }
+        result[0] = cups[(oneIdx+1) % size];
+        result[1] = cups[(oneIdx+2) % size];
+        return result;
     }
 }
