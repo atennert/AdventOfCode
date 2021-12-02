ea78bf7aa03c83f8f55ac9d6764b7121cc5321d2
Andreas Tennert <mail@andreas-tennert.de>
2021-12-02 18:10:15 +0100

    adapt day 2 2021 for shebang

--- 2021/day2/day2.kts
+++ 2021/day2/day2.kts
@@ -1,28 +1,26 @@
-import java.nio.file.Files
-import java.nio.file.Path
+#!/usr/bin/env kscript
 
 var horizontal = 0
 var vertical = 0
 var vertical2 = 0
 var aim = 0
-Files.lines(Path.of("input.txt"))
-    .forEach {
-        val input = it.split(" ")
-        val value = input[1].toInt()
-        when (input[0]) {
-            "forward" -> {
-                horizontal += value
-                vertical2 += aim * value
-            }
-            "up" -> {
-                vertical -= value
-                aim -= value
-            }
-            "down" -> {
-                vertical += value
-                aim += value
-            }
+while (true) {
+    val input = readlnOrNull()?.split(" ") ?: break
+    val value = input[1].toInt()
+    when (input[0]) {
+        "forward" -> {
+            horizontal += value
+            vertical2 += aim * value
+        }
+        "up" -> {
+            vertical -= value
+            aim -= value
+        }
+        "down" -> {
+            vertical += value
+            aim += value
         }
     }
+}
 println(horizontal * vertical)
 println(horizontal * vertical2)
