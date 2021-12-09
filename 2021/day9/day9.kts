6506d210d76404c1b0e99da37bd64e22d8c10f1b
Andreas Tennert <mail@andreas-tennert.de>
2021-12-09 11:53:51 +0100

    beautify day 9 2021

--- 2021/day9/day9.kts
+++ 2021/day9/day9.kts
@@ -6,7 +6,7 @@ while (true) {
     heightMap.add(line.map { it.digitToInt() })
 }
 
-fun isLowPoint(heightMap: List<List<Int>>, x: Int, y: Int): Boolean {
+fun isLowPoint(x: Int, y: Int): Boolean {
     val height = heightMap[y][x]
     val northPoint = heightMap.getOrNull(y - 1)?.getOrNull(x)
     val eastPoint = heightMap.getOrNull(y)?.getOrNull(x + 1)
@@ -27,7 +27,7 @@ class Point(val x: Int, val y: Int, val height: Int) {
 val lowPoints = mutableListOf<Point>()
 for (y in 0 until heightMap.size) {
     for (x in 0 until heightMap[y].size) {
-        if (isLowPoint(heightMap, x, y)) {
+        if (isLowPoint(x, y)) {
             lowPoints.add(Point(x, y, heightMap[y][x]))
         }
     }
@@ -35,37 +35,27 @@ for (y in 0 until heightMap.size) {
 
 println("low point sum: ${lowPoints.sumOf { it.height + 1 }}")
 
-fun getPointOrNull(heightMap: List<List<Int>>, x: Int, y: Int): Point? {
+fun getPointOrNull(x: Int, y: Int): Point? {
     return heightMap.getOrNull(y)?.getOrNull(x)?.let { Point(x, y, it) }
 }
 
-fun computeBasin(heightMap: List<List<Int>>, point: Point): Int {
-    return computeBasin1(heightMap, point, mutableSetOf()).size
-}
-
-fun computeBasin1(
-    heightMap: List<List<Int>>, point: Point, checkedPoints: MutableSet<Point>
-): Set<Point> {
+fun computeBasin(point: Point, checkedPoints: MutableSet<Point>): Set<Point> {
     checkedPoints.add(point)
-    val x = point.x
-    val y = point.y
-    val northPoint = getPointOrNull(heightMap, x, y - 1)
-    val eastPoint = getPointOrNull(heightMap, x + 1, y)
-    val southPoint = getPointOrNull(heightMap, x, y + 1)
-    val westPoint = getPointOrNull(heightMap, x - 1, y)
-    for (p in listOf(northPoint, eastPoint, southPoint, westPoint)) {
-        if (p != null && p.height < 9 && !checkedPoints.contains(p)) {
-            computeBasin1(heightMap, p, checkedPoints)
-        }
-    }
+    val northPoint = getPointOrNull(point.x, point.y - 1)
+    val eastPoint = getPointOrNull(point.x + 1, point.y)
+    val southPoint = getPointOrNull(point.x, point.y + 1)
+    val westPoint = getPointOrNull(point.x - 1, point.y)
+
+    listOf(northPoint, eastPoint, southPoint, westPoint)
+        .filter { it != null && it.height < 9 && !checkedPoints.contains(it) }
+        .forEach { computeBasin(it!!, checkedPoints) }
+
     return checkedPoints
 }
 
-val biggestBasins = mutableListOf<Int>()
-for (point in lowPoints) {
-    biggestBasins.add(computeBasin(heightMap, point))
-}
-biggestBasins.sort()
-biggestBasins.reverse()
+val biggestBasins = lowPoints
+    .map { computeBasin(it, mutableSetOf()).size }
+    .sorted()
+    .takeLast(3)
 
-println("combined basin size: ${biggestBasins[0] * biggestBasins[1] * biggestBasins[2]}")
+println("combined basin size: ${biggestBasins.reduce { a, b -> a * b }}")
