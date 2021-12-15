1f65d69a3c926546512394bf09091cf3dd8e8cba
Andreas Tennert <mail@andreas-tennert.de>
2021-12-15 15:14:03 +0100

    refactor day 15 2021

--- 2021/day15/day15.kts
+++ 2021/day15/day15.kts
@@ -1,14 +1,12 @@
 #!/usr/bin/env kscript
 
-import java.util.PriorityQueue
-
 val grid = mutableListOf<IntArray>()
 while (true) {
     val line = readlnOrNull()?.map { it.digitToInt() }?.toIntArray() ?: break
     grid.add(line)
 }
 
-fun nextActions(position: Pair<Int, Int>, factor: Int): List<Pair<Int, Int>> {
+fun getActions(position: Pair<Int, Int>, factor: Int): List<Pair<Int, Int>> {
     val (x, y) = position
     return listOf(Pair(1, 0), Pair(0, 1), Pair(-1, 0), Pair(0, -1))
         .filterNot { (xa, ya) ->
@@ -16,44 +14,50 @@ fun nextActions(position: Pair<Int, Int>, factor: Int): List<Pair<Int, Int>> {
         }
 }
 
-fun g(nextGoal: Pair<Int, Int>): Int {
-    val costBonus = Math.floorDiv(nextGoal.first, grid.size) + Math.floorDiv(nextGoal.second, grid.size)
-    return (grid[nextGoal.second % grid.size][nextGoal.first % grid.size] + costBonus - 1) % 9 + 1
+fun getActionCost(nextPosition: Pair<Int, Int>, nextAction: Pair<Int, Int>): Int {
+    val tileBonus = nextPosition.first / grid.size + nextPosition.second / grid.size
+    return (grid[nextPosition.second % grid.size][nextPosition.first % grid.size] + tileBonus - 1) % 9 + 1
 }
 
-fun h(currentPosition: Pair<Int, Int>, nextPosition: Pair<Int, Int>) = 0
-//    abs(currentPosition.first - nextPosition.first) + abs(currentPosition.second - nextPosition.second)
+fun h(nextPosition: Pair<Int, Int>, goal: Pair<Int, Int>) = 0
+//    abs(nextPosition.first - goal.first) + abs(nextPosition.second - goal.second)
 
 operator fun Pair<Int, Int>.plus(other: Pair<Int, Int>): Pair<Int, Int> =
     Pair(this.first + other.first, this.second + other.second)
 
 class BranchItem(val branchCost: Int, val currentPosition: Pair<Int, Int>, val action: Pair<Int, Int>)
 
-fun aStar(start: Pair<Int, Int>, goal: Pair<Int, Int>, factor: Int): Pair<List<Pair<Int, Int>>, Int> {
-    val queue = PriorityQueue<Pair<Int, Pair<Int, Int>>> { e1, e2 -> e1.first.compareTo(e2.first) }
+fun aStar(
+    start: Pair<Int, Int>,
+    goal: Pair<Int, Int>,
+    h: (Pair<Int, Int>, Pair<Int, Int>) -> Int,
+    getActionCost: (Pair<Int, Int>, Pair<Int, Int>) -> Int,
+    getActions: (Pair<Int, Int>) -> List<Pair<Int, Int>>
+): Pair<List<Pair<Int, Int>>, Int> {
+    val queue = mutableListOf<Pair<Int, Pair<Int, Int>>>()
     queue.add(Pair(0, start))
     val visited = mutableSetOf(start)
     val branch = mutableMapOf<Pair<Int, Int>, BranchItem>()
     var found = false
 
-    while (!queue.isEmpty()) {
-        val item = queue.poll()
-        val currentPosition = item.second
-        val currentCost = if (currentPosition == start) 0 else branch[currentPosition]?.branchCost ?: 0
+    while (queue.isNotEmpty()) {
+        val currentPosition = queue.removeFirst().second
+        val currentCost = branch[currentPosition]?.branchCost ?: 0 // g
 
         if (currentPosition == goal) {
             found = true
             break
         }
-        for (nextAction in nextActions(currentPosition, factor)) {
+        for (nextAction in getActions(currentPosition)) {
             val nextPosition = currentPosition + nextAction
-            val branchCost = currentCost + g(nextPosition)
-            val queueCost = branchCost + h(currentPosition, nextPosition)
+            val branchCost = currentCost + getActionCost(nextPosition, nextAction)
+            val queueCost = branchCost + h(nextPosition, goal)
 
             if (!visited.contains(nextPosition)) {
                 visited.add(nextPosition)
                 branch[nextPosition] = BranchItem(branchCost, currentPosition, nextAction)
                 queue.add(Pair(queueCost, nextPosition))
+                queue.sortBy { it.first }
             }
         }
     }
@@ -73,5 +77,9 @@ fun aStar(start: Pair<Int, Int>, goal: Pair<Int, Int>, factor: Int): Pair<List<P
     return Pair(path, pathCost)
 }
 
-println("lowest risk: ${aStar(Pair(0, 0), Pair(grid.size - 1, grid.size - 1), 1).second}")
-println("lowest risk large: ${aStar(Pair(0, 0), Pair(grid.size * 5 - 1, grid.size * 5 - 1), 5).second}")
+val start = Pair(0, 0)
+val goal1 = Pair(grid.size - 1, grid.size - 1)
+val goal2 = Pair(grid.size * 5 - 1, grid.size * 5 - 1)
+
+println("lowest risk: ${aStar(start, goal1, ::h, ::getActionCost) { getActions(it, 1) }.second}")
+println("lowest risk large: ${aStar(start, goal2, ::h, ::getActionCost) { getActions(it, 5)}.second}")
