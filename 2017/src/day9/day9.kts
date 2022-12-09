8fe599a6437e030e86b28dd83a2b1f773760c65b
Andreas Tennert <mail@andreas-tennert.de>
2022-12-09 15:46:10 +0100

    solve day 9 2017

---
+++ 2017/src/day9/day9.kts
@@ -0,0 +1,32 @@
+import java.nio.file.Files
+import java.nio.file.Path
+
+var score = 0
+var level = 0
+var insideTrash = false
+var ignoreNext = false
+var trash = 0
+
+Files.readString(Path.of("input.txt"))
+    .toCharArray()
+    .forEach {
+        if (insideTrash && it != '!' && !ignoreNext && it != '>') {
+            trash++
+        }
+        when {
+            ignoreNext -> ignoreNext = false
+            it == '!' -> ignoreNext = true
+            it == '>' -> insideTrash = false
+            insideTrash -> {/* noop */}
+            it == '{' -> {
+                level++
+                score += level
+            }
+            it == '}' -> level--
+            it == '<' -> insideTrash = true
+            else -> {/* noop */}
+        }
+    }
+
+println(score)
+println(trash)
