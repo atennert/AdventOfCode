a810d4946a1253429531535f80f5836be9dec5d5
Andreas Tennert <mail@andreas-tennert.de>
2022-12-09 09:38:45 +0100

    solve day 6 2017

---
+++ 2017/src/day6/day6.kts
@@ -0,0 +1,28 @@
+import java.nio.file.Files
+import java.nio.file.Path
+
+fun runCycles() {
+    states.clear()
+    while (!states.contains(memoryBanks)) {
+        states.add(memoryBanks.toList())
+        val maxBlocks = memoryBanks.maxOf { it }
+        val maxIndex = memoryBanks.indexOf(maxBlocks)
+        memoryBanks[maxIndex] = 0
+        for (i in 1..maxBlocks) {
+            val bankIndex = (maxIndex + i) % memoryBanks.size
+            memoryBanks[bankIndex] = memoryBanks[bankIndex] + 1
+        }
+    }
+}
+
+val memoryBanks = Files.readString(Path.of("input.txt"))
+    .split('\t')
+    .map { it.toInt() }
+    .toMutableList()
+val states = mutableSetOf<List<Int>>()
+
+runCycles()
+println(states.size)
+
+runCycles()
+println(states.size)
